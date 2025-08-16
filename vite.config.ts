a8d55a7e0036debc8a57851cac166fadac3a6b95
Fernando F. <117779898+fehleques@users.noreply.github.com>
2025-08-16 01:26:01 -0300

    feat: centralize style modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "${path.resolve(__dirname, 'src/styles/_variables.scss')}";
-          @import "${path.resolve(__dirname, 'src/styles/_mixins.scss')}";
-        `
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
